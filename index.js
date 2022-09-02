2184f4919d09101e6fde7cbf594c5eabd746f2aa
Xanderk <Xanderkidwell@gmail.com>
2022-09-02 15:09:01 -0700

    commit tree 1510bfba057e74e740f1bd255049e51d5d97bc9b

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// code your solution here
+function saturdayFun(task) {
+    if (!task) { return "This Saturday, I want to roller-skate!" } else {
+        return `This Saturday, I want to ${task}!`
+    };
+
+
+
+}
+
+function mondayWork(work) {
+    if (!work) {
+        return "This Monday, I will go to the office."
+    } else {
+        return "This Monday, I will work from home."
+    }
+
+}
+
+function wrapAdjective(wrap) {
+    const innerFunction = function (title = '*') {
+        return `You are ${wrap}${title}${wrap}!`;
+    };
+    return innerFunction;
+}
